dc488b84fb7420cf6d4fb4d556bc416515944038
red-mat <jesus.ing.rodriguez@gmail.com>
2023-03-18 14:29:08 -0300

    fix(components): resolve type child compoenents

--- src/components/IF/IF.tsx
+++ src/components/IF/IF.tsx
@@ -1,25 +1,29 @@
-import { type FC, type ReactElement } from "react"
+import { type ReactElement, type ReactNode } from "react"
 
+type TSentencesComp = React.FC<ISentencesProps>
 interface ISentencesProps {
-  children: React.ReactNode
+  children: ReactNode
 }
-const THEN: FC<ISentencesProps> = ({ children }) => {
-  return <>{children}</>
-}
-const ELSE: FC<ISentencesProps> = ({ children }) => {
-  return <>{children}</>
-}
+
+const THEN: TSentencesComp = ({ children }: ISentencesProps) => <>{children}</>
+const ELSE: TSentencesComp = ({ children }: ISentencesProps) => <>{children}</>
 
 interface IIFProps {
   condition: boolean
   children: [ReactElement<ISentencesProps>, ReactElement<ISentencesProps>]
 }
-function IF({ condition, children }: IIFProps): JSX.Element {
-  const [ItemA, ItemB] = children
-  const THEN = ItemA.type.name === "THEN" ? ItemA : ItemB
-  const ELSE = ItemA.type.name === "ELSE" ? ItemA : ItemB
 
-  return condition ? THEN : ELSE
+interface IIFComposition {
+  THEN: TSentencesComp
+  ELSE: TSentencesComp
+}
+const IF: React.FC<IIFProps> & IIFComposition = ({ condition, children }) => {
+  const [SentA, SentB] = children
+
+  const CompTHEN = SentA.type === THEN ? SentA : SentB
+  const CompELSE = SentA.type === ELSE ? SentA : SentB
+
+  return condition ? CompTHEN : CompELSE
 }
 
 IF.THEN = THEN
